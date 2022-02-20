ce8b8c9e89ed27c031247654da994d75bc20159b
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-19 20:15:35 -0500

    update objects

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""I love geometry"""
+
 
 class square():
-    
+    """Initialize attributes to 0"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Set the attributes"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -21,7 +23,6 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
